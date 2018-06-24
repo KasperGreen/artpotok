e7b405bc966aefd1aed1aaef4717cb3709f38289
Kasper Green <kaspergreen@gmail.com>
2018-06-24 18:52:32 +0300

    portrait and mobile custom media

--- config/custom_media.config.js
+++ config/custom_media.config.js
@@ -1,6 +1,8 @@
 module.exports =                       {
   extensions:
     {
+      '--portrait': '(orientation: portrait)',
+      '--landscape': '(orientation: landscape)',
       '--mobile': '(max-width: 655px)',
       '--tablet': '(max-width: 1020px)',
       '--desktop': '(min-width: 1021px)'
