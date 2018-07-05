52ecdda49bf014174d912e1d50dcf8a53b48a843
Kasper Green <kaspergreen@gmail.com>
2018-07-05 12:41:46 +0300

    Partners

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Partners.css'
+
+export default class Partners extends Component {
+  render () {
+    return (
+      <div className='Partners'>
+
+      </div>
+    )
+  }
+}
