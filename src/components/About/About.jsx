a65be1794fb9eee8290a3d3bcd6fa75283386b11
Kasper Green <kaspergreen@gmail.com>
2018-07-26 01:51:49 +0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,18 +2,32 @@ import React, { Component } from 'react'
 import './About.css'
 import PageTitle from 'components/PageTitle/PageTitle'
 import Container from 'components/Container'
-
+import Text from 'templates'
+import userContextConnection from 'context/User/userContextConnection'
+import work_image from './images/work.png'
+@userContextConnection('user')
 export default class About extends Component {
+
   render () {
+    const {
+      props: {
+        user: {
+          is_logged_in,
+          name
+        }
+      }
+    } = this
     return (
       <div className='About'>
         <Container>
           <PageTitle>
-            О фестивале
+            Администрация В Потоке
           </PageTitle>
-          <div className='About-test'>
-            Тестовый текст о фестивале
-          </div>
+          <Text>
+            Привет, {is_logged_in ? name : 'Друг'}! Это Система Управления Потоком. Сокращённо — СУП.
+            <img className='About-image' src={work_image} alt={'Администрация'} />
+          </Text>
+
         </Container>
       </div>
     )
