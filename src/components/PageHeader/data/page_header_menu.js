aa3fd514cc7438efe1df3097b088d91d74d5025f
Kasper Green <kaspergreen@gmail.com>
2018-07-26 01:54:52 +0300

    new menu

--- src/components/PageHeader/data/page_header_menu.js
+++ src/components/PageHeader/data/page_header_menu.js
@@ -1,18 +1,14 @@
 export default [
   {
-    title: 'Участие',
-    to: '/participation'
-  },
-  {
-    title: 'Купить билет',
-    to: '/buy_ticket'
-  },
-  {
     title: 'Программа',
     to: '/program'
   },
   {
     title: 'Подробности',
     to: '/info'
-  }
+  },
+  {
+    title: 'Партнёры',
+    to: '/partners'
+  },
 ]
